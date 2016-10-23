a6066dfa30787d910282bf1a1391e6b9a29aea40
Melnikov Semen <sema.melnikov@gmail.com>
2016-10-23 20:01:38 +0300

    Add some apps and resources starting

--- Friend.java
+++ Friend.java
@@ -1,24 +1,48 @@
-/*
- * HomeFriend
- */
-
-import java.io.IOException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.concurrent.ExecutionException;
 
-
-public class Friend {
+public class FriendTest {
 
     public static void openChrome() {
         try {
             Process open = Runtime.getRuntime().exec("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void openVk() {
+        try {
+            Process open = Runtime.getRuntime().exec("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe --app=\"https://vk.com/\"");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void openMail() {
+        try {
+            Process open = Runtime.getRuntime().exec("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe --app=\"https://mail.google.com/\"");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void openTranslator() {
+        try {
+            Process open = Runtime.getRuntime().exec("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe --app=\"https://translate.google.ru/\"");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void openRep() {
+        try {
+            Process open = Runtime.getRuntime().exec("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe --app=\"https://bitbucket.org/VladislavToporov/toporov_11601\"");
         } catch (Exception ex) {
             ex.printStackTrace();
         }
@@ -34,6 +58,7 @@ public class Friend {
         String note = scanner.nextLine();
         pw.println(note);
         pw.close();
+
     }
 
     public static boolean check(String str) throws FileNotFoundException {
@@ -41,8 +66,24 @@ public class Friend {
             enableDiary();
             return true;
         }
-        else if (str.equals("Открой браузер")) {
+        else if (str.equalsIgnoreCase("Открой браузер")) {
             openChrome();
+            return true;
+        }
+        else if (str.equalsIgnoreCase("Открой вконтакте")) {
+            openVk();
+            return true;
+        }
+        else if (str.equalsIgnoreCase("Открой почту")) {
+            openMail();
+            return true;
+        }
+        else if (str.equalsIgnoreCase("Открой переводчик")) {
+            openTranslator();
+            return true;
+        }
+        else if (str.equalsIgnoreCase("Открой мой репозиторий")) {
+            openRep();
             return true;
         }
         return false;
@@ -51,6 +92,7 @@ public class Friend {
     public static void main(String [] args) throws FileNotFoundException {
         final int N = 70, M = 20;
         String [][] dictionary = new String [N][M];
+
         Scanner scan = new Scanner(new File("dictionary.txt"));
         int lineCount = 0;
         do {
@@ -58,13 +100,14 @@ public class Friend {
             dictionary[lineCount] = currentLine.split("@");
             lineCount++;
         } while (scan.hasNextLine());
+        String line;
         Scanner sc = new Scanner(System.in);
-        String line = sc.nextLine();
+        line = sc.nextLine();
         String words[], word;
         int r;
         boolean flag = false;
         Random random = new Random();
-        while (!line.equalsIgnoreCase("Пока")) { 
+        while (!line.equalsIgnoreCase("Пока")) {
             if (check(line)) {
                 System.out.println("Что-нибудь еще?");
                 line = sc.nextLine();
@@ -74,6 +117,10 @@ public class Friend {
             boolean markCheck = true;
             for (int i = 0; i < words.length; i++) {
                 word = words[i];
+                if (word.charAt(word.length() - 1) == '.' || word.charAt(word.length() - 1) == '?' ||
+                    word.charAt(word.length() - 1) == ',' || word.charAt(word.length() - 1) == '!') {
+                    word = word.substring(0, word.length() - 1);
+                }
                 flag = false;
                 for (int j = 0; j < lineCount && !flag; j++) {
                     if (word.equalsIgnoreCase(dictionary[j][0])) {
@@ -90,5 +137,7 @@ public class Friend {
             }
             line = sc.nextLine();
         }
+        r = random.nextInt(dictionary[10].length - 2);
+        System.out.println(dictionary[10][r + 1]);
     }
 }
